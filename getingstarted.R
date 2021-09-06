4d9ac4eef6c424c902a1085d6e4edb93d5d2db7d
aekuebler <89928121+aekuebler@users.noreply.github.com>
2021-09-06 11:20:48 -0400

    Add files via upload

---
+++ getingstarted.R
@@ -0,0 +1,10 @@
+getwd("~/WM Freshman Year/Data 100/Data Stored")
+t <- 1
+x <- 1:10
+y <- 10:1
+ls([1] "x" "y")
+plot(x,y,type="o")
+main = "The Path of a Running Boy",
+sub = "units of distance = meters",
+xlab = ("longitude") 
+ylab = ("latitude")
